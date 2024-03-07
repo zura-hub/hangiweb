4162da604492116fcdff6fc1f5cbfc0c34cd5a03
Think <sajaia89@gmail.com>
2024-03-07 21:09:16 +0200

    next coomit

--- hangi/settings.py
+++ hangi/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'modeltranslation',
+    'admin_soft.apps.AdminSoftDashboardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
