4fcebdba040067d87949d60ed42e37fbcaf92bfa
Think <sajaia89@gmail.com>
2024-03-07 19:26:53 +0200

    next commit

--- hangi/settings.py
+++ hangi/settings.py
@@ -128,8 +128,15 @@ MODELTRANSLATION_PREPOPULATE_LANGUAGE = 'ka'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+import os
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
