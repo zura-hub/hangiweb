97ac104b5523ef53dcbca4eff6062576d9d48332
Think <sajaia89@gmail.com>
2024-03-07 19:52:50 +0200

    next commit

--- main/apps.py
+++ main/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+
+
+class AdminSoftDashboardConfig(AppConfig):
+    name = 'admin_soft'
